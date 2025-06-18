9aa4758c546611c9435894e528a81d55772d6961
JalenLv <jalenlv543@gmail.com>
2025-06-18 14:42:07 +0800

    Updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$#" -eq 0 ]; then
-    colcon build --mixin compile-commands #ninja
+    colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON # -GNinja
 else
-    colcon build --packages-select "$@" --mixin compile-commands #ninja
+    colcon build --packages-select "$@" --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON # -GNinja
 fi
 
